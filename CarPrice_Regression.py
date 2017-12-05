0086f84f51743441ab668c28fa2a50138af70349
mamenitro <e155713@ie.u-ryukyu.ac.jp>
2017-12-05 17:37:57 +0900

    Make Basic Regression, Only use NUMBER data version.

--- CarPrice_Regression.py
+++ CarPrice_Regression.py
@@ -1,48 +1,49 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-from sklearn import datasets, linear_model
+from sklearn import linear_model
 from sklearn.metrics import mean_squared_error, r2_score
 
-
-# Load the diabetes dataset
-diabetes = pd.read_csv('datasets.csv')
-
+# Load the Autemobile dataset
+Autemobile = pd.read_csv('datasets.csv')
 
 # Use only one feature
-# diabetes_X = diabetes.data[:, np.newaxis, 2]
+# Autemobile_X = Autemobile.data[:, np.newaxis, 2]
 
-diabetes_X = diabetes.drop("price",axis=1)
-print(diabetes_X)
+Autemobile_X = Autemobile.drop("price",axis=1)
 
 # Split the data into training/testing sets
-diabetes_X_train = diabetes_X[:-20]
-diabetes_X_test = diabetes_X[-20:]
+Autemobile_X_train = Autemobile_X[:-20]
+Autemobile_X_test = Autemobile_X[-20:]
 
 # Split the targets into training/testing sets
-diabetes_y_train = diabetes.price[:-20]
-diabetes_y_test = diabetes.price[-20:]
+Autemobile_y_train = Autemobile.price[:-20]
+Autemobile_y_test = Autemobile.price[-20:]
 
 # Create linear regression object
 regr = linear_model.LinearRegression()
 
 # Train the model using the training sets
-regr.fit(diabetes_X_train, diabetes_y_train)
+regr.fit(Autemobile_X_train, Autemobile_y_train)
 
 # Make predictions using the testing set
-diabetes_y_pred = regr.predict(diabetes_X_test)
+Autemobile_y_pred = regr.predict(Autemobile_X_test)
 
 # The coefficients
 print('Coefficients: \n', regr.coef_)
 # The mean squared error
 print("Mean squared error: %.2f"
-      % mean_squared_error(diabetes_y_test, diabetes_y_pred))
+      % mean_squared_error(Autemobile_y_test, Autemobile_y_pred))
 # Explained variance score: 1 is perfect prediction
-print('Variance score: %.2f' % r2_score(diabetes_y_test, diabetes_y_pred))
+print('Variance score: %.2f' % r2_score(Autemobile_y_test, Autemobile_y_pred))
+
+print(Autemobile_y_pred)
+print(Autemobile_y_test)
+print(Autemobile_y_test - Autemobile_y_pred)
 
 # Plot outputs
-# plt.scatter(diabetes_X_test, diabetes_y_test,  color='black')
-# plt.plot(diabetes_X_test, diabetes_y_pred, color='blue', linewidth=3)
+# plt.scatter(Autemobile_X_test, Autemobile_y_test,  color='black')
+# plt.plot(Autemobile_X_test, Autemobile_y_pred, color='blue', linewidth=3)
 
 # plt.xticks(())
 # plt.yticks(())
