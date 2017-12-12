65730b3b5b519486503347266576d80cabe84904
mamenitro <e155713@ie.u-ryukyu.ac.jp>
2017-12-12 17:39:28 +0900

    交叉確認法no

--- CarPrice_Regression.py
+++ CarPrice_Regression.py
@@ -5,58 +5,71 @@ from sklearn import linear_model
 # from sklearn.metrics import mean_squared_log_error
 from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
 
-# Load the Autemobile dataset
-Autemobile = pd.read_csv('datasets.csv')
+# Load the Automobile dataset
+Automobile = pd.read_csv('datasets2.csv')
+Automobile_X = Automobile.drop("price",axis=1)
 
-Autemobile_X = Autemobile.drop("price",axis=1)
+AllAbsoluteError = []
 
-# 学習に使うデータ行とテストに使うデータ行を指定
-Autemobile_X_train = Autemobile_X[:-20]
-Autemobile_X_test = Autemobile_X[-20:]
+for i in range(len(Automobile_X)):
+    
+    # Load the Automobile dataset
+    Automobile = pd.read_csv('datasets2.csv')
+    Automobile_X = Automobile.drop("price",axis=1)
 
-# 学習に使うターゲット行とテストに使うターゲット行を指定
-Autemobile_y_train = Autemobile.price[:-20]
-Autemobile_y_test = Autemobile.price[-20:]
+    Automobile_y_test = Automobile.price.drop(i)
+    Automobile_X_test = Automobile_X.drop(i)
 
-# 線形モデルを作成
-regr = linear_model.LinearRegression()
+    # 学習に使うデータ行とテストに使うデータ行を指定
+    Automobile_y_train = Automobile.price
+    Automobile_X_train = Automobile_X
 
-# †学習†
-regr.fit(Autemobile_X_train, Autemobile_y_train)
+    # 学習に使うターゲット行とテストに使うターゲット行を指定
 
-# テストデータで予測を作成
-Autemobile_y_pred = regr.predict(Autemobile_X_test)
+    # 線形モデルを作成
+    regr = linear_model.LinearRegression()
 
-# The coefficients 係数？
-print('Coefficients: \n', regr.coef_)
+    # †学習†
+    regr.fit(Automobile_X_train, Automobile_y_train)
 
-# Explained variance score: 1 is perfect prediction
-# 分散スコア→１なら完全予測
-print('Variance score: %.2f' % r2_score(Autemobile_y_test, Autemobile_y_pred))
+    # テストデータで予測を作成
+    Automobile_y_pred = regr.predict(Automobile_X_test)
 
-# The mean absolute error
-# 平均絶対誤差
-print('Mean Absolute error: %.2f' 
-      % mean_absolute_error(Autemobile_y_test, Autemobile_y_pred))
+    # The coefficients 係数？
+    # print('Coefficients: \n', regr.coef_)
 
-# The mean squared error 
-# 平均二乗誤差
-print("Mean squared error: %.2f"
-      % mean_squared_error(Autemobile_y_test, Autemobile_y_pred))
+    # Explained variance score: 1 is perfect prediction
+    # 分散スコア→１なら完全予測
+    # print('Variance score: %.2f' % r2_score(Automobile_y_test, Automobile_y_pred))
 
+    # The mean absolute error
+    # 平均絶対誤差
+    # print('Mean Absolute error: %.2f' 
+    #      % mean_absolute_error(Automobile_y_test, Automobile_y_pred))
+    AllAbsoluteError.append(mean_absolute_error(Automobile_y_test, Automobile_y_pred))
+
+
+    # The mean squared error 
+    # 平均二乗誤差
+    # print("Mean squared error: %.2f"
+    #      % mean_squared_error(Automobile_y_test, Automobile_y_pred))
+# for n in range(len(AllAbsoluteError)) :
+#     print(AllAbsoluteError[n])
+
+print(sum(AllAbsoluteError))
 # The mean squared logarithmic error
 # 平均二乗対数誤差
 # print("Mean squared logarithmic error: %.2f" 
-#       % mean_squared_log_error(Autemobile_y_test, Autemobile_y_pred))
+#       % mean_squared_log_error(Automobile_y_test, Automobile_y_pred))
 
-print(Autemobile_y_pred)
-print(Autemobile_y_test)
-print(Autemobile_y_test - Autemobile_y_pred)
+# print(Automobile_y_pred)
+# print(Automobile_y_test)
+# print(Automobile_y_test - Automobile_y_pred)
 
 # Plot outputs
 # グラフに描画するゾーン．削除可
-# plt.scatter(Autemobile_X_test, Autemobile_y_test,  color='black')
-# plt.plot(Autemobile_X_test, Autemobile_y_pred, color='blue', linewidth=3)
+# plt.scatter(Automobile_X_test, Automobile_y_test,  color='black')
+# plt.plot(Automobile_X_test, Automobile_y_pred, color='blue', linewidth=3)
 
 # plt.xticks(())
 # plt.yticks(())
