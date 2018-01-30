143513b4a421d82e536545345769d7ac92f66134
mamenitro <e155713@ie.u-ryukyu.ac.jp>
2018-01-30 16:09:07 +0900

    コメントアウトの整理，ソース先頭にソースの概要を書きました

--- CarPrice_Regression.py
+++ CarPrice_Regression.py
@@ -1,18 +1,23 @@
-# import matplotlib.pyplot as plt
+#
+# 線形回帰モデル．
+# 評価方法はクロスバリデーション（確認交差法）．
+# なので，平均絶対誤差と平均二乗誤差は全て同じ値となる．
+#
 import numpy as np
 import pandas as pd
 from sklearn import linear_model
-# from sklearn.metrics import mean_squared_log_error
-from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
+from sklearn.metrics import mean_squared_error, mean_absolute_error
 
 #データセットの読み込み 
-Automobile = pd.read_csv('datasets.csv')
+Automobile = pd.read_csv('datasets4.csv')
+del Automobile['Unnamed: 0']
 Automobile_X = Automobile.drop("price",axis=1)
 
 #最終誤差値用の結果を格納する配列を用意
 AllAbsoluteError = []
 AllSquaredError = []
 
+# クロスバリデーション（確認交差法）
 for i in range(len(Automobile_X)):
     
     #テスト用データを１行だけ指定
@@ -32,44 +37,11 @@ for i in range(len(Automobile_X)):
     # テストデータで予測を作成
     Automobile_Y_pred = regr.predict(Automobile_X_test)
 
-    # The coefficients 係数？
-    # print('Coefficients: \n', regr.coef_)
-
-    # Explained variance score: 1 is perfect prediction
-    # 分散スコア→１なら完全予測
-    # print('Variance score: %.2f' % r2_score(Automobile_y_test, Automobile_y_pred))
-
-    # The mean absolute error
-    # 平均絶対誤差
-    # print('Mean Absolute error: %.2f' 
-    #      % mean_absolute_error(Automobile_y_test, Automobile_y_pred))
+    # 平均絶対誤差を計算して各行の結果を配列へその都度記録
     AllAbsoluteError.append(mean_absolute_error(Automobile_Y_test, Automobile_Y_pred))
-
+    # 平均二乗誤差でも同じことを
     AllSquaredError.append(np.sqrt(mean_squared_error(Automobile_Y_test, Automobile_Y_pred)))
-    # The mean squared error 
-    # 平均二乗誤差
-    # print("Mean squared error: %.2f"
-    #      % mean_squared_error(Automobile_y_test, Automobile_y_pred))
-# for n in range(len(AllAbsoluteError)) :
-#     print(AllAbsoluteError[n])
 
+# 各々の平均誤差の平均を取ってその値を出力
 print('AbsoluteError => {}'.format(sum(AllAbsoluteError)/len(AllAbsoluteError)))
 print('AquaredError  => {}'.format(sum(AllSquaredError)/len(AllSquaredError)))
-# The mean squared logarithmic error
-# 平均二乗対数誤差
-# print("Mean squared logarithmic error: %.2f" 
-#       % mean_squared_log_error(Automobile_y_test, Automobile_y_pred))
-
-# print(Automobile_y_pred)
-# print(Automobile_y_test)
-# print(Automobile_y_test - Automobile_y_pred)
-
-# Plot outputs
-# グラフに描画するゾーン．削除可
-# plt.scatter(Automobile_X_test, Automobile_y_test,  color='black')
-# plt.plot(Automobile_X_test, Automobile_y_pred, color='blue', linewidth=3)
-
-# plt.xticks(())
-# plt.yticks(())
-
-# plt.show()
--- CarPrice_Regression_origin.py
+++ CarPrice_Regression_origin.py
@@ -1,14 +1,14 @@
-#ランダムに１０行を選択．それをテストデータとしそれらを用いて評価を行う
-# import matplotlib.pyplot as plt
+#
+# ランダムに１０行を選択．それをテストデータとしそれらを用いて評価を行う
+# 
 from numpy.random import *
 import numpy as np
 import pandas as pd
 from sklearn import linear_model
-# from sklearn.metrics import mean_squared_log_error
 from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
 
 #シード値を指定
-seed(100)
+seed(200)
 
 #データセットの読み込み 
 Automobile = pd.read_csv('datasets4.csv')
@@ -44,24 +44,6 @@ regr.fit(Automobile_X_train, Automobile_Y_train)
 # テストデータで予測を作成
 Automobile_Y_pred = regr.predict(Automobile_X_test)
 
-# The coefficients 係数？
-# print('Coefficients: \n', regr.coef_)
-
-# Explained variance score: 1 is perfect prediction
-# 分散スコア→１なら完全予測
-# print('Variance score: %.2f' % r2_score(Automobile_y_test, Automobile_y_pred))
-
-# The mean absolute error
-# 平均絶対誤差
-# print('Mean Absolute error: %.2f' 
-#      % mean_absolute_error(Automobile_y_test, Automobile_y_pred))
-    # The mean squared error 
-    # 平均二乗誤差
-    # print("Mean squared error: %.2f"
-    #      % mean_squared_error(Automobile_y_test, Automobile_y_pred))
-# for n in range(len(AllAbsoluteError)) :
-#     print(AllAbsoluteError[n])
-
 #平均絶対誤差と平均二乗誤差を出すよ
 print("AbsoluteError => {}".format(mean_absolute_error(Automobile_Y_test, Automobile_Y_pred)))
 print("SquaredError  => {}".format(np.sqrt(mean_squared_error(Automobile_Y_test, Automobile_Y_pred))))
@@ -71,23 +53,3 @@ print("SquaredError  => {}".format(np.sqrt(mean_squared_error(Automobile_Y_test,
 #AllSquaredError.append(np.sqrt(mean_squared_error(Automobile_Y_test, Automobile_Y_pred)))
 #print('AbsoluteError => {}'.format(sum(AllAbsoluteError)/len(AllAbsoluteError)))
 #print('AquaredError  => {}'.format(sum(AllSquaredError)/len(AllSquaredError)))
-
-
-# The mean squared logarithmic error
-# 平均二乗対数誤差
-# print("Mean squared logarithmic error: %.2f" 
-#       % mean_squared_log_error(Automobile_y_test, Automobile_y_pred))
-
-# print(Automobile_y_pred)
-# print(Automobile_y_test)
-# print(Automobile_y_test - Automobile_y_pred)
-
-# Plot outputs
-# グラフに描画するゾーン．削除可
-# plt.scatter(Automobile_X_test, Automobile_y_test,  color='black')
-# plt.plot(Automobile_X_test, Automobile_y_pred, color='blue', linewidth=3)
-
-# plt.xticks(())
-# plt.yticks(())
-
-# plt.show()
